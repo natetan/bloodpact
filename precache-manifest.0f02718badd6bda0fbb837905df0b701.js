73b4c0de95323f827002b124543965e236b1fbe9
Yulong Tan <yulongtan67@gmail.com>
2019-04-15 22:28:45 -0700

    Updates

---
+++ precache-manifest.0f02718badd6bda0fbb837905df0b701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d495928d63cad3eaa062",
+    "url": "/bloodpact/static/css/main.0ef5168f.chunk.css"
+  },
+  {
+    "revision": "d495928d63cad3eaa062",
+    "url": "/bloodpact/static/js/main.82e948f8.chunk.js"
+  },
+  {
+    "revision": "306b6dd823b50240d4e9",
+    "url": "/bloodpact/static/js/runtime~main.eb593853.js"
+  },
+  {
+    "revision": "adbcbd5ef22cf53f6dbf",
+    "url": "/bloodpact/static/css/2.f6f02e21.chunk.css"
+  },
+  {
+    "revision": "adbcbd5ef22cf53f6dbf",
+    "url": "/bloodpact/static/js/2.58dbb71c.chunk.js"
+  },
+  {
+    "revision": "ca45ed47c39e1d468cb26edc21deb22f",
+    "url": "/bloodpact/index.html"
+  }
+];
