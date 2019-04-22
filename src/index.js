48e5edfbc30e30dfe69f974aefbf6bd1aeea8b9e
Yulong Tan <yulongtan67@gmail.com>
2019-04-21 17:26:33 -0700

    Add firebase config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAAEdmaSO46PAOxsxE14b3DhwuokLzgieE",
+  authDomain: "blood-pact-674be.firebaseapp.com",
+  databaseURL: "https://blood-pact-674be.firebaseio.com",
+  projectId: "blood-pact-674be",
+  storageBucket: "blood-pact-674be.appspot.com",
+  messagingSenderId: "113963888427"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <HashRouter basename={process.env.PUBLIC_URL}>
