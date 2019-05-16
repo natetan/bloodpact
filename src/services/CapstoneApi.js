2b109be3225ac8d17ddb57b30360f57ec8136c52
Yulong Tan <yulongtan67@gmail.com>
2019-05-15 17:31:45 -0700

    Add functions to call the api

--- src/services/CapstoneApi.js
+++ src/services/CapstoneApi.js
@@ -1,6 +1,57 @@
 import fetch from 'node-fetch'
 
+const baseUrl = 'https://infocapstone2019-server.herokuapp.com';
+
 export async function getDrives(zipcode) {
-  let zips = await fetch(`https://infocapstone2019-server.herokuapp.com/drives/${zipcode}`);
-  return zips.json();
+  let res = await fetch(`${baseUrl}/drives/${zipcode}`);
+  return res.json();
+}
+
+export async function getAllGroups() {
+  let res = await fetch(`${baseUrl}/groups/all`);
+  return res.json();
+}
+
+export async function getGroup(groupName) {
+  let res = await fetch(`${baseUrl}/groups/${groupName}`);
+  return res.json();
+}
+
+export function getUserGroups(uid) {
+  let res = await fetch(`${baseUrl}/users/${uid}/groups`);
+  return res.json();
+}
+
+export async function createGroup(name, uid, firstName, lastName, pintsDonated = 3) {
+  let members = {};
+  members[uid] = {
+    firstName: firstName,
+    lastName: lastName
+  };
+  let options = {
+    method: 'POST',
+    body: {
+      name: name,
+      friendlyName: name.toLowerCase().split(' ').join('-'),
+      createdDate: new Date().toISOString().substring(0, 10),
+      members: members,
+      pintsDonated: pintsDonated
+    }
+  };
+
+  let res = await fetch(`${baseUrl}/groups/create`, options);
+  return res.json();
+}
+
+export function joinGroup(groupName, uid, firstName, lastName) {
+  let options = {
+    method: 'PUT',
+    body: {
+      uid: uid,
+      firstName: firstName,
+      lastName: lastName,
+    }
+  };
+  let res = await fetch(`${baseUrl}/groups/${groupName}/join`, options);
+  return res.json();
 }
